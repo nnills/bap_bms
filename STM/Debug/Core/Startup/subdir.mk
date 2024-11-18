e97880310f3a4eeae74193c2c9dce49706dda43c
tjgeschiere <e12302743@student.tuwien.ac.at>
2024-11-18 11:32:57 +0100

    ik haat git

--- STM/Debug/Core/Startup/subdir.mk
+++ STM/Debug/Core/Startup/subdir.mk
@@ -16,7 +16,7 @@ OBJS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m7 -g3 -DDEBUG -c -I"C:/Users/tjges/OneDrive/Documents/bap_bms/STM/Core/Inc" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m7 -g3 -DDEBUG -c -I"C:/Users/tjges/OneDrive/Documents/bap_bms/STM/Core/Inc" -I"C:/Users/tjges/OneDrive/Documents/bap_bms/STM/AD_code" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
 clean: clean-Core-2f-Startup
 
